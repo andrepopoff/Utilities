c4d6ab33a545f27ba519444af5ff83807428ccb7
andrepopoff <andrepopoff@gmail.com>
2018-11-23 15:34:16 +0400

    Wrapped it in a function and added a login and password for the VK account

--- vk_albums_shifter/vk_albums_shifter.py
+++ vk_albums_shifter/vk_albums_shifter.py
@@ -1,17 +1,20 @@
 import vk
 
-MY_USER_ID = '7978511'
-APP_ID = '6273721'
 GROUP_ID = '-54705114'
 
-with open('login_details.txt', 'r') as f:
-    login, password = [line.rstrip() for line in f]
 
-vk_session = vk.AuthSession(app_id=APP_ID, user_login=login, user_password=password, scope='groups, photos')
-vk_api = vk.API(vk_session)
+def reorder_albums(group_id, app_id='6273721'):
+    login = input('Введите логин ВК: ')
+    password = input('Введите пароль: ')
 
-try:
-    vk_api.photos.reorderAlbums(v='5.0', owner_id=GROUP_ID, album_id=239117930, before=180568944)
-    print('Операция успешно выполнена')
-except vk.exceptions.VkAPIError:
-    print('Ошибка VkAPIError')
+    vk_session = vk.AuthSession(app_id=app_id, user_login=login, user_password=password, scope='groups, photos')
+    vk_api = vk.API(vk_session)
+
+    try:
+        vk_api.photos.reorderAlbums(v='5.0', owner_id=group_id, album_id=239330927, before=180568944)
+        print('Операция успешно выполнена')
+    except vk.exceptions.VkAPIError:
+        print('Ошибка VkAPIError')
+
+if __name__ == '__main__':
+    reorder_albums(GROUP_ID)
