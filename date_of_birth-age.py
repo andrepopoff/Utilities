86228fedcca78ef63e50ccbe758711b59332e36a
andrepopoff <andrepopoff@gmail.com>
2018-11-24 01:05:27 +0400

    Fixed defects

--- date_of_birth-age.py
+++ date_of_birth-age.py
@@ -4,8 +4,6 @@ from datetime import date
 bdate = date(1988, 3, 27)
 today = date.today()
 age = today.year - bdate.year
-if today.month < bdate.month:
-    age -= 1
-elif today.month == bdate.month and today.day < bdate.day:
+if today.month < bdate.month or today.month == bdate.month and today.day < bdate.day:
     age -= 1
 print(age)
