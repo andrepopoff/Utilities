1de090d7f3bc0b313f7cdecb43d51edab84e01d0
andrepopoff <andrepopoff@gmail.com>
2018-11-23 16:40:14 +0400

    The working function that defines the group id is ready

--- vk_albums_shifter/vk_albums_shifter.py
+++ vk_albums_shifter/vk_albums_shifter.py
@@ -1,21 +1,22 @@
 import vk
 import re
 
-GROUP_ID = '-54705114'
 APP_ID = '6273721'
 
 
 def reorder_albums(vk_api, group_id):
     try:
-        vk_api.photos.reorderAlbums(v='5.0', owner_id=group_id, album_id=239117516, before=180568944)
+        vk_api.photos.reorderAlbums(v='5.0', owner_id=group_id, album_id=239340196, before=180568944)
         print('Операция успешно выполнена')
     except vk.exceptions.VkAPIError:
         print('Ошибка VkAPIError')
 
 
-def learn_group_id(link):
-    # vk_response = vk_api
-    pass
+def learn_group_id(vk_api):
+    link = input('Введите ссылку на группу: ')
+    group_name = re.findall('vk.com/([\S]+$)', link)[0]
+    group_id = vk_api.groups.getById(v='5.0', group_id=group_name)[0]['id']
+    return -group_id
 
 
 def connect_with_vk(app_id):
@@ -27,7 +28,5 @@ def connect_with_vk(app_id):
 
 if __name__ == '__main__':
     vk_api = connect_with_vk(APP_ID)
-    link = input('Введите ссылку на группу: ')
-    learn_group_id(link)
-
-    reorder_albums(vk_api, GROUP_ID)
+    group_id = learn_group_id(vk_api)
+    reorder_albums(vk_api, group_id)
