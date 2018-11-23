a6712b305e53428c229990496e88e39c3888bb97
andrepopoff <andrepopoff@gmail.com>
2018-11-23 16:03:14 +0400

    Separated the authorization function, which is connected with vk api.

--- vk_albums_shifter/vk_albums_shifter.py
+++ vk_albums_shifter/vk_albums_shifter.py
@@ -5,24 +5,29 @@ GROUP_ID = '-54705114'
 APP_ID = '6273721'
 
 
-def reorder_albums(login, password, group_id, app_id):
-    vk_session = vk.AuthSession(app_id=app_id, user_login=login, user_password=password, scope='groups, photos')
-    vk_api = vk.API(vk_session)
-
+def reorder_albums(vk_api, group_id):
     try:
-        vk_api.photos.reorderAlbums(v='5.0', owner_id=group_id, album_id=239330927, before=180568944)
+        vk_api.photos.reorderAlbums(v='5.0', owner_id=group_id, album_id=239117516, before=180568944)
         print('Операция успешно выполнена')
     except vk.exceptions.VkAPIError:
         print('Ошибка VkAPIError')
 
 
-def get_info_about_the_group(link):
+def learn_group_id(link):
+    # vk_response = vk_api
     pass
 
 
-if __name__ == '__main__':
+def connect_with_vk(app_id):
     login = input('Введите логин ВК: ')
     password = input('Введите пароль: ')
-    link = input('Введите ссылку на группу: ')
+    vk_session = vk.AuthSession(app_id=app_id, user_login=login, user_password=password, scope='groups, photos')
+    return vk.API(vk_session)
 
-    reorder_albums(login, password, GROUP_ID, APP_ID)
+
+if __name__ == '__main__':
+    vk_api = connect_with_vk(APP_ID)
+    link = input('Введите ссылку на группу: ')
+    learn_group_id(link)
+
+    reorder_albums(vk_api, GROUP_ID)
