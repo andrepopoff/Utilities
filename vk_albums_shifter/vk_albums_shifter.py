a6e713defc20ff4073537db0ed9e11ccc576d3e6
andrepopoff <andrepopoff@gmail.com>
2018-12-13 18:53:17 +0400

    Now you do not need to constantly write the login and password (added access_token)

--- vk_albums_shifter/vk_albums_shifter.py
+++ vk_albums_shifter/vk_albums_shifter.py
@@ -2,55 +2,61 @@
 This script changes the order of the albums in the group on vk.com.
 The script is useful, who has commercial groups on vk.com with a large number of albums.
 This eliminates the routine of dragging albums with the PC mouse.
+----------------------------------------------------------------------
+Before you start, you need to get access_token.
+Just follow:
+https://oauth.vk.com/authorize?client_id=6273721&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=groups,photos,offline&response_type=token&v=5.52
+and copy the data from the access_token parameter of url to the access_token variable.
+-----------------------------------------------------------------------
+By default, access_token = ''. Until you get access_token, the script will not work.
 """
 
-import vk
 import re
 import sys
 import time
+import requests
+
 
 APP_ID = '6273721'
+access_token = ''
 
 
-def reorder_albums(vk_api, group_id, album_ids_to_move, id_for_point_album):
+def reorder_albums(group_id, album_ids_to_move, id_for_point_album):
     """
     Changes the order of albums
     """
     print('...Выполняется перемещение альбомов...')
-    try:
-        for album_id in album_ids_to_move:
-            time.sleep(2)
-            vk_api.photos.reorderAlbums(v='5.0', owner_id=group_id, album_id=album_id, before=id_for_point_album)
+
+    for album_id in album_ids_to_move:
+        time.sleep(2)
+        print(group_id, 'group')
+        response = requests.get('https://api.vk.com/method/photos.reorderAlbums', params={'v': 5.52,
+                                                                                          'owner_id': group_id,
+                                                                                          'album_id': album_id,
+                                                                                          'before': id_for_point_album,
+                                                                                          'access_token': access_token})
+        json_response = response.json()
+        if json_response['response']:
             print('Альбом {} перемещен'.format(album_id))
-    except vk.exceptions.VkAPIError:
-        print('Ошибка VkAPIError')
+        else:
+            print('Код ошибки: {}. {}'.format(json_response['error']['error_code'], json_response['error']['error_msg']))
 
 
-def pull_group_id(vk_api, link):
+def pull_group_id(link):
     """
     Link to group --> group id
     """
     try:
         group_name = re.findall('vk.com/([\S]+$)', link)[0]
-        group_id = vk_api.groups.getById(v='5.0', group_id=group_name)[0]['id']
+        response = requests.get('https://api.vk.com/method/groups.getById', params={'v': 5.52,
+                                                                                    'group_id': group_name,
+                                                                                    'access_token': access_token})
+        group_id = response.json()['response'][0]['id']
+        # group_id = vk_api.groups.getById(v='5.0', group_id=group_name)[0]['id']
     except IndexError:
         print('Вы ввели неверную ссылку на группу!')
         sys.exit()
     return -group_id
-
-
-def connect_with_vk(app_id):
-    """
-    Authorization on vk.com
-    """
-    try:
-        login = input('Введите логин ВК: ')
-        password = input('Введите пароль: ')
-        vk_session = vk.AuthSession(app_id=app_id, user_login=login, user_password=password, scope='groups, photos')
-    except vk.exceptions.VkAuthError:
-        print('Неправильный логин или пароль!')
-        sys.exit()
-    return vk.API(vk_session)
 
 
 def pull_album_ids(albums_to_move, album_pointer, mode):
@@ -101,9 +107,8 @@ def script_parameters():
 
 
 if __name__ == '__main__':
-    vk_api = connect_with_vk(APP_ID)
     parameters_dict = script_parameters()
-    group_id = pull_group_id(vk_api, parameters_dict['group_link'])
+    group_id = pull_group_id(parameters_dict['group_link'])
     album_ids_to_move, id_for_point_album = pull_album_ids(parameters_dict['albums_to_move'],
                                                            parameters_dict['album_pointer'], parameters_dict['mode'])
-    reorder_albums(vk_api, group_id, album_ids_to_move, id_for_point_album)
+    reorder_albums(group_id, album_ids_to_move, id_for_point_album)
