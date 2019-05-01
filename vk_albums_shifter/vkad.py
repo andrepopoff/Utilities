0d13c254bdc063e4a55eb367be17247480f1723f
andrepopoff <andrepopoff@gmail.com>
2019-05-01 11:12:11 +0800

    Created vkad.py

---
+++ vk_albums_shifter/vkad.py
@@ -0,0 +1,12 @@
+import sys
+
+APP_ID = '6273721'
+API_PATH = 'https://api.vk.com/method/'
+API_VERSION = 5.52
+access_token = ''  # Before you start, you need to get access_token
+
+
+
+
+if __name__ == '__main__':
+    print(sys.argv)
