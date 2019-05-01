676312fb620e408f6cba8ac174a8263238a19e78
andrepopoff <andrepopoff@gmail.com>
2019-05-01 15:11:44 +0800

    vkad.py: now we can place a post on the wall

--- vk_albums_shifter/vkad.py
+++ vk_albums_shifter/vkad.py
@@ -1,7 +1,8 @@
 import sys
 import requests
+import re
 
-APP_ID = '6273721'
+APP_ID = '6967460'
 API_PATH = 'https://api.vk.com/method/'
 API_VERSION = 5.52
 access_token = ''  # Before you start, you need to get access_token
@@ -15,8 +16,7 @@ def get_group_id():
 
 def get_all_albums(group_id):
     response = requests.get(API_PATH + 'photos.getAlbums', params={'v': API_VERSION, 'owner_id': group_id,
-                                                                   'access_token': access_token, 'need_covers': 1,
-                                                                   'photo_sizes': 1})
+                                                                   'access_token': access_token})
     response_dict = response.json()
     if response_dict.get('response'):
         return response_dict['response']['items']
@@ -31,9 +31,14 @@ if __name__ == '__main__':
         for album in all_albums:
             if album['id'] != 140958045:
                 title = album['title']
+                stopped = re.findall(r'\bстоп\b', title, flags=re.I)
+                if stopped:
+                    break
                 description = album['description']
-
-                for photo in album['sizes']:
-                    if photo['type'] == 'z':
-                        src = photo['src']
-                        break
+                message = title + '\n\n' + description if description else title
+                attachment = 'album{}_{}'.format(group_id, album['id'])
+                response = requests.get(API_PATH + 'wall.post', params={'v': API_VERSION, 'owner_id': group_id,
+                                                                        'access_token': access_token, 'from_group': 1,
+                                                                        'message': message, 'attachments': attachment})
+                print(response.json())
+            break
