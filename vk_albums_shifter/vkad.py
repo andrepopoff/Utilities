2a82c964bcf93e254f828b09a8de4f10742c8c83
andrepopoff <andrepopoff@gmail.com>
2019-05-01 11:28:34 +0800

    Wrote get_group_id func

--- vk_albums_shifter/vkad.py
+++ vk_albums_shifter/vkad.py
@@ -6,13 +6,13 @@ API_VERSION = 5.52
 access_token = ''  # Before you start, you need to get access_token
 
 
-def get_group():
+def get_group_id():
     if len(sys.argv) == 2:
-        group_id_by_city = {'syk': 123, 'uht': 123}
-        group_id = group_id_by_city[sys.argv[1]]
+        group_id_by_city = {'syk': -22426905, 'uht': -79007931}
+        return group_id_by_city.get(sys.argv[1])
 
 
 if __name__ == '__main__':
-    group = get_group()
+    group = get_group_id()
     if group:
         pass
